5484415f0a74625fbf98de8cdae31751efef5c93
Ben Kovacs (Laptop) <bencekovacs@outlook.com>
2018-06-18 19:46:44 +1000

    Completed UI of Login Screen

--- Assets/Equipment-Booking-System/Equipment-Booking-System/Login Screen.Designer.vb
+++ Assets/Equipment-Booking-System/Equipment-Booking-System/Login Screen.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frmLogin
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,95 +20,89 @@ Partial Class frmLogin
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.lblLoanDetails = New System.Windows.Forms.Label()
         Me.txtUsername = New System.Windows.Forms.TextBox()
         Me.txtPassword = New System.Windows.Forms.TextBox()
         Me.btnLogin = New System.Windows.Forms.Button()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblClose = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'lblLoanDetails
         '
         Me.lblLoanDetails.AutoSize = True
         Me.lblLoanDetails.Font = New System.Drawing.Font("Segoe UI", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblLoanDetails.ForeColor = System.Drawing.Color.LightCyan
-        Me.lblLoanDetails.Location = New System.Drawing.Point(200, 57)
-        Me.lblLoanDetails.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblLoanDetails.ForeColor = System.Drawing.Color.Azure
+        Me.lblLoanDetails.Location = New System.Drawing.Point(133, 37)
         Me.lblLoanDetails.Name = "lblLoanDetails"
-        Me.lblLoanDetails.Size = New System.Drawing.Size(193, 71)
+        Me.lblLoanDetails.Size = New System.Drawing.Size(128, 47)
         Me.lblLoanDetails.TabIndex = 23
         Me.lblLoanDetails.Text = "LOGIN"
         '
         'txtUsername
         '
-        Me.txtUsername.BackColor = System.Drawing.Color.LightCyan
+        Me.txtUsername.BackColor = System.Drawing.Color.Azure
         Me.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.txtUsername.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtUsername.ForeColor = System.Drawing.Color.DeepSkyBlue
-        Me.txtUsername.Location = New System.Drawing.Point(76, 206)
-        Me.txtUsername.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtUsername.Location = New System.Drawing.Point(51, 134)
         Me.txtUsername.Name = "txtUsername"
-        Me.txtUsername.Size = New System.Drawing.Size(436, 36)
+        Me.txtUsername.Size = New System.Drawing.Size(291, 24)
         Me.txtUsername.TabIndex = 24
         Me.txtUsername.Text = "USERNAME"
         Me.txtUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'txtPassword
         '
-        Me.txtPassword.BackColor = System.Drawing.Color.LightCyan
+        Me.txtPassword.BackColor = System.Drawing.Color.Azure
         Me.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.txtPassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtPassword.ForeColor = System.Drawing.Color.DeepSkyBlue
-        Me.txtPassword.Location = New System.Drawing.Point(76, 309)
-        Me.txtPassword.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtPassword.Location = New System.Drawing.Point(51, 201)
         Me.txtPassword.Name = "txtPassword"
-        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.txtPassword.Size = New System.Drawing.Size(436, 36)
+        Me.txtPassword.Size = New System.Drawing.Size(291, 24)
         Me.txtPassword.TabIndex = 25
         Me.txtPassword.Text = "PASSWORD"
         Me.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'btnLogin
         '
-        Me.btnLogin.BackColor = System.Drawing.Color.LightCyan
+        Me.btnLogin.BackColor = System.Drawing.Color.Azure
         Me.btnLogin.Font = New System.Drawing.Font("Segoe UI", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnLogin.ForeColor = System.Drawing.Color.DeepSkyBlue
-        Me.btnLogin.Location = New System.Drawing.Point(386, 406)
-        Me.btnLogin.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnLogin.Location = New System.Drawing.Point(257, 264)
         Me.btnLogin.Name = "btnLogin"
-        Me.btnLogin.Size = New System.Drawing.Size(129, 48)
+        Me.btnLogin.Size = New System.Drawing.Size(86, 31)
         Me.btnLogin.TabIndex = 26
         Me.btnLogin.Text = "SUBMIT"
         Me.btnLogin.UseVisualStyleBackColor = False
         '
-        'Label1
+        'lblClose
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.ForeColor = System.Drawing.Color.LightCyan
-        Me.Label1.Location = New System.Drawing.Point(565, 9)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(31, 32)
-        Me.Label1.TabIndex = 27
-        Me.Label1.Text = "X"
+        Me.lblClose.AutoSize = True
+        Me.lblClose.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.lblClose.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblClose.ForeColor = System.Drawing.Color.Azure
+        Me.lblClose.Location = New System.Drawing.Point(366, 9)
+        Me.lblClose.Name = "lblClose"
+        Me.lblClose.Size = New System.Drawing.Size(27, 30)
+        Me.lblClose.TabIndex = 27
+        Me.lblClose.Text = "X"
         '
         'frmLogin
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.DeepSkyBlue
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
-        Me.ClientSize = New System.Drawing.Size(608, 525)
-        Me.Controls.Add(Me.Label1)
+        Me.ClientSize = New System.Drawing.Size(405, 341)
+        Me.Controls.Add(Me.lblClose)
         Me.Controls.Add(Me.btnLogin)
         Me.Controls.Add(Me.txtPassword)
         Me.Controls.Add(Me.txtUsername)
         Me.Controls.Add(Me.lblLoanDetails)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
-        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.Name = "frmLogin"
         Me.Text = "Login_Screen"
         Me.ResumeLayout(False)
@@ -120,5 +114,5 @@ Partial Class frmLogin
     Friend WithEvents txtUsername As TextBox
     Friend WithEvents txtPassword As TextBox
     Friend WithEvents btnLogin As Button
-    Friend WithEvents Label1 As Label
+    Friend WithEvents lblClose As Label
 End Class
--- Assets/Equipment-Booking-System/Equipment-Booking-System/Login Screen.vb
+++ Assets/Equipment-Booking-System/Equipment-Booking-System/Login Screen.vb
@@ -1,34 +1,45 @@
 ﻿Public Class frmLogin
-    'Switching forms on the click of the submit button
+    'Switching forms on the click of the submit button TEMP
     Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
         frmMainMenu.Show()
         Me.Hide()
     End Sub
     'Close button label thingo
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+    Private Sub lblClose_Click(sender As Object, e As EventArgs) Handles lblClose.Click
         Me.Close()
     End Sub
-    Private Sub txtUsername_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtUsername.GotFocus
+
+    '
+    '
+    'Make username and password fields have relevent descriptions in them when input
+    '
+    '
+
+    ''"Username" disappear when field is clicked
+    Private Sub txtUsername_MouseClick(sender As Object, e As MouseEventArgs) Handles txtUsername.MouseClick
         If txtUsername.Text = "USERNAME" Then
             txtUsername.Text = ""
         End If
     End Sub
-
-    Private Sub TxtUsername_LostFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtUsername.LostFocus
-
+    ''"Username" reappear when field is clicked off and left blank
+    Private Sub txtUsername_LostFocus(sender As Object, e As EventArgs) Handles txtUsername.LostFocus
         If txtUsername.Text = "" Then
             txtUsername.Text = "USERNAME"
         End If
     End Sub
-    Private Sub txtPassword_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtUsername.GotFocus
-        If txtPassword.Text = "PASSWORD" Then
-            txtPassword.Text = ""
+    ''"Password reappear in normal characters when field is blank and clicked off
+    Private Sub txtPassword_LostFocus(sender As Object, e As EventArgs) Handles txtPassword.LostFocus
+        If txtPassword.Text = "" Then
+            txtPassword.PasswordChar = ""
+            txtPassword.Text = "PASSWORD"
         End If
     End Sub
-    Private Sub txtPassword_LostFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtUsername.LostFocus
+    ''Password disappear when user clicks and character type is set to secure password characters
+    Private Sub txtPassword_MouseClick(sender As Object, e As MouseEventArgs) Handles txtPassword.MouseClick
 
-        If txtUsername.Text = "" Then
-            txtUsername.Text = "PASSWORD"
+        If txtPassword.Text = "PASSWORD" Then
+            txtPassword.PasswordChar = "*"
+            txtPassword.Text = ""
         End If
     End Sub
 End Class
