c3ed1ef20adfbdc29c2179a86c7fe0b5298f2422
Ben Kovacs (Laptop) <bencekovacs@outlook.com>
2018-06-18 22:26:34 +1000

    Added new database and connected it to the login screen, rudimental and not linked to other forms yet

--- Assets/Equipment-Booking-System/Equipment-Booking-System/Login Screen.vb
+++ Assets/Equipment-Booking-System/Equipment-Booking-System/Login Screen.vb
@@ -1,9 +1,15 @@
-﻿Public Class frmLogin
-    'Switching forms on the click of the submit button TEMP
-    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
-        frmMainMenu.Show()
-        Me.Hide()
-    End Sub
+﻿Imports System.Data.SqlClient
+Public Class frmLogin
+
+
+
+    ''Switching forms on the click of the submit button TEMP
+    'Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+    '    frmMainMenu.Show()
+    '    Me.Hide()
+    'End Sub
+
+
     'Close button label thingo
     Private Sub lblClose_Click(sender As Object, e As EventArgs) Handles lblClose.Click
         Me.Close()
@@ -42,4 +48,68 @@
             txtPassword.Text = ""
         End If
     End Sub
+    ''
+    ''
+    ''PASSWORDING
+    ''
+    ''
+
+    Dim con As New SqlClient.SqlConnection
+    Dim da As New SqlClient.SqlDataAdapter
+    Dim ds As New DataSet
+    Dim sqlquery As String
+    Private Sub frmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        con.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Bence\Documents\SWD\12SWD-SAT\Assets\Equipment-Booking-System\Equipment-Booking-System\Users.mdf;Integrated Security=True"
+
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Try
+            'checking if the username and password field is null
+
+            If Len(Trim(txtUsername.Text)) = 0 Then
+                MessageBox.Show("Enter the user name", "Input Error !", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Exit Sub
+                txtUsername.Focus()
+            End If
+            If Len(Trim(txtPassword.Text)) = 0 Then
+                MessageBox.Show("Enter the password", "Input Error !", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Exit Sub
+                txtPassword.Focus()
+            End If
+
+            'executing SQL Query for retrieving the username and password from the database table
+
+            con.Open()
+            sqlquery = "SELECT * FROM UserInfo WHERE Username='" & txtUsername.Text & "' and Password='" & txtPassword.Text & "' "
+            da = New SqlClient.SqlDataAdapter(sqlquery, con)
+            da.Fill(ds, "UserInfo")
+            If ds.Tables("UserInfo").Rows.Count <> 0 Then
+                MessageBox.Show("Login succeed", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                MessageBox.Show("Invalid user name and password", "Access denied", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+            con.Close()
+
+            'clear()    'calling clear method here
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Exception generated", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    '' declaring a method for clearing the controls
+    'Public Sub clear()
+    '    txtPassword.Clear()
+    '    txtUsername.Clear()
+    'End Sub
+
+    '' code for show password
+    'Private Sub chkshowpass_CheckedChanged(sender As Object, e As EventArgs) Handles chkshowpass.CheckedChanged
+    '    If chkshowpass.Checked = True Then
+    '        txtPass.UseSystemPasswordChar = False
+    '    Else
+    '        txtPass.UseSystemPasswordChar = True
+    '    End If
+    'End Sub
 End Class
